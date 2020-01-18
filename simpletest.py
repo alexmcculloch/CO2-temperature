98d66a001aad595effd3c411de4b4648538c105a
peter-shoes <bcm73@drexel.edu>
2020-01-18 17:01:53 -0500

    collated code

--- simpletest.py
+++ simpletest.py
@@ -1,24 +1,7 @@
-#!/usr/bin/python
-# Copyright (c) 2014 Adafruit Industries
-# Author: Tony DiCola
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+import time
+import board
+import busio
+import adafruit_sgp30
 
 # Can enable debug output by uncommenting:
 #import logging
@@ -43,11 +26,21 @@ def c_to_f(c):
 # exposed with SCL = P9_19 and SDA = P9_20.
 sensor = MCP9808.MCP9808()
 
+i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
 # Optionally you can override the address and/or bus number:
 #sensor = MCP9808.MCP9808(address=0x20, busnum=2)
 
 # Initialize communication with the sensor.
 sensor.begin()
+
+sgp30 = adafruit_sgp30.Adafruit_SGP30(i2c)
+
+print("SGP30 serial #", [hex(i) for i in sgp30.serial])
+
+sgp30.iaq_init()
+sgp30.set_iaq_baseline(0x8973, 0x8aae)
+
+elapsed_sec = 0
 
 # Loop printing measurements every second.
 print('Press Ctrl-C to quit.')
@@ -55,3 +48,10 @@ while True:
 	temp = sensor.readTempC()
 	print('Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(temp, c_to_f(temp)))
 	time.sleep(1.0)
+	print("eCO2 = %d ppm \t TVOC = %d ppb" % (sgp30.eCO2, sgp30.TVOC))
+    time.sleep(1)
+    elapsed_sec += 1
+    if elapsed_sec > 10:
+        elapsed_sec = 0
+        print("**** Baseline values: eCO2 = 0x%x, TVOC = 0x%x"
+              % (sgp30.baseline_eCO2, sgp30.baseline_TVOC))
